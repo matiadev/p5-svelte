eb9f6d50d43ebb18978112592c536bafe9153fb1
matiadev <mattcroat.dev@gmail.com>
2026-03-17 20:57:29 +0100

    chore: update example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,20 @@
 <script lang="ts">
-	import Canvas, { type Sketch } from '$lib/index.js';
+	import P5Sketch, { type Sketch } from '$lib/index.js';
 
 	let diameter = $state(100);
 
-	// @ts-expect-error cursed library
-	// const addons = [() => import('p5.sound')];
-
-	// import type p5 from 'p5';
-	// interface SoundMethods {
-	// 	loadSound: (path: string) => Promise<{
-	// 		play: () => void;
-	// 		stop: () => void;
-	// 		isPlaying: () => boolean;
-	// 	}>;
-	// }
-
-	const sketch: Sketch = (s) => {
-		// const p = s as p5 & SoundMethods;
-		// let sound: Awaited<ReturnType<SoundMethods['loadSound']>>;
-		s.setup = async () => {
-			// sound = await p.loadSound('./sfx.mp3');
-			s.createCanvas(400, 400);
+	const sketch: Sketch = (p5) => {
+		p5.setup = () => {
+			p5.createCanvas(400, 400);
 		};
-		s.draw = () => {
-			s.background(200);
-			s.circle(s.width / 2, s.height / 2, diameter);
+		p5.draw = () => {
+			p5.background(200);
+			p5.circle(p5.width / 2, p5.height / 2, diameter);
 		};
-		// s.mousePressed = () => {
-		// 	return sound.isPlaying() ? sound.stop() : sound.play();
-		// };
 	};
 </script>
 
-<Canvas {sketch} />
+<P5Sketch {sketch} />
 
 <label>
 	diameter
